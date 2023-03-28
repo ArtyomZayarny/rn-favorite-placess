423b35b88513c0ff5b94c194a6e376fd3875111d
Artem Zaiarnyi <artem_zaiarnyi@epam.com>
2023-03-28 20:58:06 +0300

    Added styles for header

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { Text } from 'react-native';
 import { IconButton } from './components/ui/IconButton';
+import { Colors } from './constants/colors';
 import { AddPlace } from './screens/AddPlace';
 import { AllPlaces } from './screens/AllPlaces';
 
@@ -13,11 +13,22 @@ export default function App() {
     <>
       <StatusBar />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: Colors.primary500,
+            },
+            headerTintColor: Colors.gray700,
+            contentStyle: {
+              backgroundColor: Colors.gray700,
+            },
+          }}
+        >
           <Stack.Screen
             name="AllPlaces"
             component={AllPlaces}
             options={({ navigation }) => ({
+              title: 'Your Favorite Places',
               headerRight: ({ tintColor }) => (
                 <IconButton
                   icon="add"
@@ -28,7 +39,13 @@ export default function App() {
               ),
             })}
           />
-          <Stack.Screen name="AddPlace" component={AddPlace} />
+          <Stack.Screen
+            name="AddPlace"
+            component={AddPlace}
+            options={{
+              title: 'Add New Place',
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
